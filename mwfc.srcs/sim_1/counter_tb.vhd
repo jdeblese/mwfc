a3366908896dd63f07b1e5fdddad0de4e6d2f19e
Jan-willem De Bleser <jw@thescrapyard.org>
2014-08-28 22:02:43 -0400

    Refactoring of counter, still needs proper overflow

--- mwfc.srcs/sim_1/counter_tb.vhd
+++ mwfc.srcs/sim_1/counter_tb.vhd
@@ -3,12 +3,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 use IEEE.NUMERIC_STD.ALL;
 
-use work.constants.all;
-
 entity counter_tb is
 end counter_tb;
 
 architecture Behavioral of counter_tb is
+    constant precision : integer := 13;
+    constant measureinterval : integer := 256;  -- 8191;
+    constant inputbits : integer := precision + 5;
+    constant timerbits : integer := 18;
+
     signal clk, clk2 : std_logic := '0';
     constant clk_period : time := 10 ns;
 --    constant clk2_period : time := 1.25 ns;
@@ -17,188 +20,32 @@ architecture Behavioral of counter_tb is
 --    constant clk2_period : time := 25 ns;
 --    constant clk2_period : time := 24.985 ns;
 --    constant clk2_period : time := 12.3107226 ns;
-    constant clk2_period : time := clk_period * 10.0;
+    constant clk2_period : time := clk_period * 3.14159;
 
     signal rst : std_logic;
 
-    signal tcount, isync1 : unsigned(timerbits-1 downto 0);
-    signal icount, isync0 : unsigned(inputbits-1 downto 0);
+    signal tcount : unsigned(timerbits-1 downto 0);
+    signal icount : unsigned(inputbits-1 downto 0);
 
-	signal syncia, syncib : unsigned(inputbits-1 downto 0);
-    
-    signal scaling : signed(nscalestages-1 downto 0);
-    signal divbusy, divoverflow, divstrobe : std_logic;
-
-	signal ratio : unsigned(precision-1 downto 0);
-	signal final : unsigned(precision downto 0);
-    
-    type A is array (0 to 31) of integer;
-    constant digits : A := (
-		0, 0, 0, 0, 0, 0, 0, 0, 0,
-		2,
-		3, 3, 3, 3,
-        4, 4, 4,
-        5, 5, 5,
-        6, 6, 6, 6,
-        7, 7, 7,
-        8, 8, 8,
-        9, 9 );
-	signal order : signed(7 downto 0);
-	type B is array(0 to 31) of unsigned(precision + 2 downto 0);
-	constant corrections : B := (
-		x"0000", x"0000", x"0000", x"0000", x"0000", x"0000", x"0000", x"0000", x"0000",
-		x"3200",
-		x"FA00", x"7D00", x"3E80", x"1F40",
-		x"9C40", x"4E20", x"2710",
-		x"C350", x"61A8", x"30D4",
-		x"F424", x"7A12", x"3D09", x"1E85",
-		x"9897", x"4C4B", x"2626",
-		x"BEBC", x"5F5E", x"2FAF",
-		x"EE6B", x"7736" );
-    
-    signal bcd : std_logic_vector(15 downto 0);
-    signal gpustrobe : std_logic;
-
-    signal dispen : std_logic_vector(3 downto 0);
-	signal dispc : std_logic_vector(7 downto 0);
-	signal dispa : std_logic_vector(3 downto 0);
-
-	signal tmp : unsigned(corrections(0)'range);
+    signal divstrobe : std_logic;
 begin
 
-    dispen <= "1111" when divoverflow = '0' else "0000";
-
-    disp : entity work.driveseg
-        port map (
-            data => bcd,
-            seg_c => dispc,
-            seg_a => dispa,
-            en => dispen,
-            clk => clk,
-            rst => rst );
-
-    conv : entity work.hex2bcd
-        generic map (
-            precision => final'length,
-            width => 16,
-            bits => 4 )
-        port map (
-            hex => final,
-            bcd => bcd,
-            strobe => gpustrobe,
-            rst => rst,
-            clk => clk );
-
-    process(clk)
-        variable counter : unsigned(2 downto 0);
-    begin
-        if rising_edge(clk) then
-            gpustrobe <= '0';
-            if divbusy = '1' then
-                counter := "100";
-            else
-                if counter = "001" then
-                    gpustrobe <= '1';
-                end if;
-                if counter > "0" then
-                    counter := counter - "1";
-                end if;
-            end if;
-        end if;
-    end process;
-
-	process(clk,rst)
-        variable C0 : unsigned(corrections(0)'range);
-		variable S0 : integer;
-		variable F1, F2, F3 : unsigned(ratio'length + corrections(0)'length - 1 downto 0);
-        variable O1, O2 : signed(order'range);
-	begin
-		if rst = '1' then
-			S0 := 0;
-		elsif rising_edge(clk) then
-            -- Pipeline stage 3
-			if divoverflow = '0' then
-				F3 := shift_right(F2, 4);
-				-- Round based on highest truncated bit
-				if F3(0) = '1' then
-					final <= F3(final'high+1 downto 1) + "1";
-				else
-					final <= F3(final'high+1 downto 1);
-				end if;
-			end if;
-            order <= O2;
-
-            -- Pipeline stage 2
-			if divoverflow = '0' then
-				if F1 < shift_left(to_unsigned(1000, F1'length),5) then
-					-- Five bits, because retaining one to round
---					F2 := F1 * 10;
-					F2 := shift_left(F1,1) + shift_left(F1,3);
-					O2 := O1 - 1;
-                else
-                    F2 := F1;
-                    O2 := O1;
-				end if;
-			end if;
-
-            -- Pipeline stage 1
-			O1 := to_signed(8, order'length) - digits(S0);
-			if divoverflow = '0' then
-				-- corrections is shifted up precision+3 bits
-                -- Pipeline the access to corrections
-                -- Multiply is a slow operation
-				F1 := shift_right(ratio * C0, precision + 3 - 1 - 4);
-            end if;
-
-            -- Pipeline stage 0
-			S0 := -to_integer(scaling);
-            C0 := corrections(S0);
-			tmp <= C0;
-		end if;
-	end process;
-
-
-	-- Count the number of timer and input tics in the given
-	-- measurement interval, taking a whole number of input tics.
-    stage1 : entity work.counter 
+    -- Count the number of timer and input tics in the given
+    -- measurement interval, taking a whole number of input tics.
+    dut : entity work.counter 
         generic map (
             Tlen => tcount'length,
             Ilen => icount'length,
-			measureinterval => measureinterval )
+            measureinterval => measureinterval )
         port map (
             timer => clk,
             input => clk2,
             tcount => tcount,
             icount => icount,
+--          overflow => open,
             enable => '1',
-			strobe => divstrobe,
-            rst => rst);
-
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            isync1 <= (others => '0');
-            isync1(isync0'range) <= isync0;
-            isync0 <= icount;
-        end if;
-    end process;
-
-    -- Divide M by N
-    stage2 : entity work.fpdiv
-        generic map (
-            size => tcount'length,
-            precision => ratio'length,
-            pscale => scaling'length )
-        port map (
-            dividend => isync1,
-            divisor => tcount,
-            quotient => ratio,
-            scale => scaling,
-            busy => divbusy,
-            overflow => divoverflow,
             strobe => divstrobe,
-            clk => clk,
-            rst => rst );
+            rst => rst);
 
     clk <= not clk after clk_period/2; 
     clk2 <= not clk2 after clk2_period/2; 
--- mwfc.srcs/sources_1/counter.vhd
+++ mwfc.srcs/sources_1/counter.vhd
@@ -5,50 +5,64 @@ use IEEE.NUMERIC_STD.ALL;
 entity counter is
     Generic ( Tlen : integer;
               Ilen : integer;
-			  measureinterval : integer );
+              measureinterval : integer );
     Port ( timer : in STD_LOGIC;
            input : in STD_LOGIC;
            tcount : out UNSIGNED (Tlen-1 downto 0);
            icount : out UNSIGNED (Ilen-1 downto 0);
---		   overflow : out STD_LOGIC;
+--           overflow : out STD_LOGIC;
            enable : in STD_LOGIC;
-		   strobe : out STD_LOGIC;
+           strobe : out STD_LOGIC;
            rst : in  STD_LOGIC);
 end counter;
 
 architecture Behavioral of counter is
+    type states is (ST_WAIT, ST_COUNTING, ST_FINISHING);
+
+    -- *** input-based signals ***
     signal input_en, input_en_next : std_logic;
-    signal M, icount_int, icount_next : unsigned (icount'range);
+    signal M : unsigned (icount'range);
+    signal icount_int, icount_next : unsigned (icount'range);  -- Main counter
 
+    signal iover, iover_next : std_logic;  -- Overflow of the 'input' counter
+
+    signal istate, istate_next : states;
+
+    -- *** timer-based signals ***
     signal timer_en, timer_en_next : std_logic;
-    signal N, tcount_int, tcount_next : unsigned (tcount'range);
-    
+    signal N : unsigned (tcount'range);
+    signal tcount_int, tcount_next : unsigned (tcount'range);  -- Main counter
+    signal sync_ien : std_logic_vector(1 downto 0);  -- Synchronizer for 'input_en'
+    signal tover, tover_next: std_logic;  -- Overflow of the 'timer' counter
+
+    signal tstate, tstate_next : states;
+
     signal done, done_next : std_logic;
+    signal strobe_next : std_logic;
 
-	signal sync_ien : std_logic_vector(1 downto 0);
-
-	signal tover, tover_next, iover, iover_next : std_logic;
-
-	signal strobe_next : std_logic;
 begin
 
     icount <= icount_int;
     tcount <= tcount_int;
 
---	overflow <= tover or iover;
+--    overflow <= tover or iover;
 
-	process(rst,timer)
-	begin
-		if rst = '1' then
-			sync_ien <= (others => '0');
-		elsif rising_edge(timer) then
-			sync_ien <= sync_ien(0) & input_en;
-		end if;
-	end process;
+    -- 'input_en' needs to be available in the 'timer' domain so that
+    -- the 'timer' counter knows when to start and stop counting
+    process(rst,timer)
+    begin
+        if rst = '1' then
+            sync_ien <= (others => '0');
+        elsif rising_edge(timer) then
+            sync_ien <= sync_ien(0) & input_en;
+        end if;
+    end process;
 
-    comb : process(enable, input_en, sync_ien, icount_int, timer_en, tcount_int, done, M, N, tover, iover)
+    -- Main FSM-type programming of the counter
+    comb : process(enable, input_en, sync_ien, icount_int, timer_en, tcount_int, done, M, N, tover, iover, istate, tstate)
         variable input_en_new, timer_en_new, done_new : std_logic;
-		variable tover_new, iover_new : std_logic;
+        variable tover_new, iover_new : std_logic;
+        variable istate_new, tstate_new : states;
     begin
         input_en_new := input_en;
         timer_en_new := timer_en;
@@ -57,84 +71,93 @@ begin
         tcount_next <= tcount_int;
         icount_next <= icount_int;
 
-		tover_new := tover;
-		iover_new := iover;
+        tover_new := tover;
+        iover_new := iover;
 
-		strobe_next <= '0';
-        
-		if timer_en = '1' and N + "1" = "0" then
-			tover_new := '1';
-		end if;
+        strobe_next <= '0';
 
-		if input_en = '1' and M + "1" = "0" then
-			iover_new := '1';
-		end if;
+        istate_new := istate;
+        tstate_new := tstate;
+        
+        -- Detect overflows
+        if timer_en = '1' and N + "1" = "0" then
+            tover_new := '1';
+        end if;
 
-		-- Start the 'input' counter on its next rising edge, if permitted
-		-- DANGER: crosses clock domain
-		--     input 'timer_en' and 'done' is in the 'timer' domain
-		--     input 'enable' is in an unknown domain
-		--     output 'input_en' is in the 'input' domain
-        if input_en = '0' and timer_en = '0' and done = '0' then
-            input_en_new := '1';
+        if input_en = '1' and M + "1" = "0" then
+            iover_new := '1';
         end if;
-        
-		-- If the 'input' counter is on, start the 'timer' counter on its
-		-- next rising edge, if not already done so
-        if sync_ien(1) = '1' then
-            timer_en_new := '1';
-        end if;
-        
-		-- Indicate when the minimum measurement interval has been reached
-		-- Subtract 2 tics to compensate for the 'input_en' synchronizer
-        if N = to_unsigned(measureinterval - 2, N'length) then
-            done_new := '1';
-        end if;
-        
-		-- If we've measured long enough, turn off the 'input' counter on
-		-- its next rising edge, and latch out the counter value
-		-- DANGER: crosses clock domain
-		--     input done is in the 'timer' domain
-		--     outputs are all in the 'input' domain
-		--     impractical to sync to 'input', as 'input' is slow vs 'timer'
-		--         if synced, icount >= 3
-        if done = '1' and input_en = '1' then
-            input_en_new := '0';
-            icount_next <= M + "1";
-			iover_new := '0';
-        end if;
-        
-		-- If the 'input' counter has been turned off, turn off the 'timer'
-		-- counter on its next rising edge and latch out the value
-        if timer_en = '1' and sync_ien(1) = '0' then
-            timer_en_new := '0';
-            tcount_next <= N;
-			tover_new := '0';
-        end if;
-        
-		-- If timer_en is off, disable the measurement interval flag
-        if done = '1' and sync_ien(1) = '0' and timer_en = '0' then
-            done_new := '0';
-			strobe_next <= '1';
-        end if;
-        
+
+        case istate is
+            when ST_WAIT =>
+                -- DANGER: crosses clock domain
+                --     'timer_en' and 'done' is in the 'timer' domain
+                if timer_en = '0' and done = '0' then
+                    istate_new := ST_COUNTING;
+                    -- 'input' couter is started on rising edge of 'input'
+                    input_en_new := '1';
+                end if;
+            when ST_COUNTING =>
+                -- DANGER: crosses clock domain
+                --     input done is in the 'timer' domain
+                if done = '1' then
+                    istate_new := ST_WAIT;
+                    input_en_new := '0';     -- Disable the counter
+                    icount_next <= M + "1";  -- Latch out the counter result
+                    iover_new := '0';        -- Clear any overflow
+                end if;
+            when others =>
+        end case;
+
+        case tstate is
+            when ST_WAIT =>
+                if sync_ien(1) = '1' then
+                    tstate_new := ST_COUNTING;
+                    -- 'timer' couter is started on rising edge of 'timer'
+                    timer_en_new := '1';
+                end if;
+            when ST_COUNTING =>
+                if N = to_unsigned(measureinterval - 2, N'length) then
+                    tstate_new := ST_FINISHING;
+                    -- Indicate when the minimum measurement interval has been reached
+                    -- Subtract 2 tics to compensate for the 'input_en' synchronizer
+                    done_new := '1';
+                end if;
+            when ST_FINISHING =>
+                -- If the timer's off, signal done and valid output
+                if timer_en = '0' then
+                    done_new := '0';
+                    strobe_next <= '1';
+                -- Otherwise, turn off the timer when the input counter is off
+                elsif sync_ien(1) = '0' then
+                    tstate_new := ST_WAIT;
+                    timer_en_new := '0';     -- Disable the counter
+                    tcount_next <= N + "1";  -- Latch out the counter result
+                    tover_new := '0';        -- Clear any overflow
+                end if;
+            when others =>
+        end case;
+
         input_en_next <= input_en_new;
         timer_en_next <= timer_en_new;
         done_next <= done_new;
-		tover_next <= tover_new;
-		iover_next <= iover_new;
+        tover_next <= tover_new;
+        iover_next <= iover_new;
+        istate_next <= istate_new;
+        tstate_next <= tstate_new;
     end process;
 
-	-- Processes synchronous to the 'timer' clock
+    -- Processes synchronous to the 'timer' clock
     tmem : process(rst, timer)
     begin
         if rst = '1' then
             timer_en <= '0';
             done <= '0';
             N <= (others => '0');
-            tcount_int <= (others => '1');
-			tover <= '0';
-			strobe <= '0';
+            tcount_int <= (others => '1');  -- Nonzero, avoids 1/0 fault
+            tover <= '0';
+            strobe <= '0';
+            tstate <= ST_WAIT;
         elsif rising_edge(timer) then
             if timer_en = '1' then
                 N <= N + "1";
@@ -144,19 +167,21 @@ begin
             timer_en <= timer_en_next;
             done <= done_next;
             tcount_int <= tcount_next;
-			tover <= tover_next;
-			strobe <= strobe_next;
+            tover <= tover_next;
+            strobe <= strobe_next;
+            tstate <= tstate_next;
         end if;
     end process;
 
-	-- Processes synchronous to the 'input' clock
+    -- Processes synchronous to the 'input' clock
     imem : process(rst, input)
     begin
         if rst = '1' then
             input_en <= '0';
             M <= (others => '0');
             icount_int <= (others => '0');
-			iover <= '0';
+            iover <= '0';
+            istate <= ST_WAIT;
         elsif rising_edge(input) then
             if input_en = '1' then
                 M <= M + "1";
@@ -165,7 +190,8 @@ begin
             end if;
             input_en <= input_en_next;
             icount_int <= icount_next;
-			iover <= iover_next;
+            iover <= iover_next;
+            istate <= istate_next;
         end if;
     end process;
 
